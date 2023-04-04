eda791788f2003d942eb5a627eda79024db6fd50
Hephaestus20 <85035055+Hephaestus20@users.noreply.github.com>
2023-04-04 21:34:10 +0300

    Update profile.js

--- commands/invite/profile.js
+++ commands/invite/profile.js
@@ -4,18 +4,18 @@ const db = require("quick.db");
 exports.run = async (client, message, args) => {
   const user = message.mentions.users.first() || message.author;
 
-  // Number of users invited to the server
+
   const entryCount = db.get(`davet_${message.guild.id}_${user.id}`) || 0;
   
-  // Number of users who have left the server
+
   const exitCount = db.get(`cikis_${message.guild.id}_${user.id}`) || 0;
   
-  // Bonus number of invites
+
   const bonusCount = db.get(`bonus_${message.guild.id}_${user.id}`) || 0;
   
   const totalInvites = entryCount + bonusCount - exitCount;
 
-  // User information and invite count message
+ 
   const embed = new Discord.MessageEmbed()
     .setAuthor(user.tag)
     .setColor("#7289DA")
